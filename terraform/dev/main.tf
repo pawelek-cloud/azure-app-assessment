f7b5eccc613a2b62a28e525e3b7e894e5e12c445
Paweł Nowak <pawelnowak@MacBook-Pro-Pawe.local>
2025-11-04 21:06:48 +0100

    Initial commit

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -71,6 +71,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
   resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.postgres.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled = true
 
   depends_on = [
     azurerm_virtual_network.vnet,
